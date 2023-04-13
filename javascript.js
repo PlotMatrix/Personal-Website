bd56231f9f494c3297b70e78b992b44c5b01fdaa
Fayez <fayezalajam@gmail.com>
2023-04-13 16:06:15 -0700

    Completed criteria for assignment: added contact page with form, linked it to other pages, fixed footer display issue on mobile devices, added longer engaging welcome message on load.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,12 @@
 window.onload = function() {
-    alert("Hello, world! Thanks for visiting my website, have a nice day!");
-  };
+  const greeting = "Hello, and welcome to my website!";
+  const message1 = "I'm so glad you stopped by today.";
+  const message2 = "I hope you find the information here helpful and informative.";
+  const message3 = "If you have any questions, don't hesitate to reach out!";
+  const message4 = "You can find my contact information on the Contact page.";
+  const message5 = "Thank you for visiting, and have a wonderful day!";
+  
+  const fullMessage = `${greeting}\n\n${message1}\n\n${message2}\n\n${message3}\n\n${message4}\n\n${message5}`;
+  
+  alert(fullMessage);
+};
